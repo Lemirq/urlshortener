37780cef0e7ffd248a53eb7f75d47e607e2fdaf6
Vihaan Sharma <sharmavihaan190@gmail.com>
2023-08-04 15:39:30 -0400

    FINAL

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
 const dns = require('dns');
-const { readFileSync, writeFileSync } = require('fs');
+const urlparser = require('url');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+const client = new MongoClient(process.env.MONGO_URI);
+const db = client.db('urlshortner');
+const urls = db.collection('urls');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -19,53 +23,33 @@ app.get('/', function (req, res) {
 	res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.listen(port, function () {
-	console.log(`Listening on port ${port}`);
-});
-
-const path = './sitelist.json';
-const jsonString = readFileSync(path);
-const parsedSitelist = JSON.parse(jsonString);
-
-app.post('/api/shorturl', (req, res) => {
+// Your first API endpoint
+app.post('/api/shorturl', function (req, res) {
+	console.log(req.body);
 	const url = req.body.url;
-
-	dns.lookup(url, { all: true }, (err, addresses) => {
-		if (err) {
-			console.log(err);
-		}
-		if (addresses) {
-			// check if url is already in sitelist
-			// if it is, return the index
-			// if not, add it to the sitelist and return the index
-
-			const urlIndex = Object.keys(parsedSitelist).find((key) => parsedSitelist[key] === url);
-			if (urlIndex) {
-				res.json({ original_url: url, short_url: urlIndex });
-			} else {
-				const newIndex = Object.keys(parsedSitelist).length + 1;
-				parsedSitelist[newIndex] = url;
-
-				const newJsonString = JSON.stringify(parsedSitelist);
-
-				writeFileSync(path, newJsonString);
-				res.json({ original_url: url, short_url: newIndex });
-			}
-		} else {
+	const dnslookup = dns.lookup(urlparser.parse(url).hostname, async (err, address) => {
+		if (!address) {
 			res.json({ error: 'Invalid URL' });
+		} else {
+			const urlCount = await urls.countDocuments({});
+			const urlDoc = {
+				url,
+				short_url: urlCount,
+			};
+
+			const result = await urls.insertOne(urlDoc);
+			console.log(result);
+			res.json({ original_url: url, short_url: urlCount });
 		}
 	});
 });
 
-app.get('/api/shorturl/:num', (req, res) => {
-	readFileSync(path);
+app.get('/api/shorturl/:short_url', async (req, res) => {
+	const shorturl = req.params.short_url;
+	const urlDoc = await urls.findOne({ short_url: +shorturl });
+	res.redirect(urlDoc.url);
+});
 
-	const num = req.params.num;
-	const url = parsedSitelist[num];
-
-	if (url) {
-		res.redirect('https://' + url);
-	} else {
-		res.json({ error: 'No short URL found for the given input' });
-	}
+app.listen(port, function () {
+	console.log(`Listening on port ${port}`);
 });
